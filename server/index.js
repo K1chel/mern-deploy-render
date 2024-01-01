8e35a5f7e8c7011f689796ad24b290618e487dad
Your Name <cherevko022@gmail.com>
2024-01-01 13:59:50 -0500

    cors edit

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://mern-server-wfe9.onrender.com",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 mongoose
   .connect(MONGO_URI)
